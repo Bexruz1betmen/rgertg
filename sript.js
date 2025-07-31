7a3352350512cfe30b999788d2cc00dfe7b85ab4
Bexruz1betmen <brawlacc1236@gmail.com>
2025-07-31 15:13:25 +0500

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,16 @@
+let gg = prompt ('Имя ')
+console.log( gg);
+
+let gk = prompt ('Отчество')
+console.log( gk);
+
+let ga = prompt ('Фамилия')
+console.log( ga);
+
+let gs = prompt ('нечинчи илсан укам')
+console.log( gs);
+
+let gd = prompt ('махала нма')
+console.log( gd);
+
+alert('оркенга караб ур, я заежаю за табой ')
